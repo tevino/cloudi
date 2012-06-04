da862b2cce8f41c4f55bee12fb6d31370094d56e
Tevin <mail2tevin@gmail.com>
2012-06-04 14:35:48 +0800

    better way for getting current dir

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-chmod +x ./cloudi.py
-current_dir=$(dirname -- $(readlink -f -- "$0")) 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+chmod +x ${CWD}/cloudi.py
 echo \#Start alias for Cloudi >>~/.bashrc
-echo alias\ d\="$current_dir/cloudi.py" >>~/.bashrc 
+echo alias\ d\="$CWD/cloudi.py" >>~/.bashrc 
 echo \#End alias for Cloudi >>~/.bashrc
 echo Installation finished.
