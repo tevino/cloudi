2a0effce3723c7c8991d8dc8a4f8fa61f0f4e6b2
Tevin Zhang <mail2tevin@gmail.com>
2013-07-29 20:54:51 +0800

    pop version, remove long_description

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 
 setup(
     name='cloudi',
-    version='0.4.2',
+    version='0.5.4',
     author='Tevin Zhang',
     author_email='mail2tevin@gmail.com',
     packages=['cloudi', ],
     scripts=['cloudi/d', ],
     url='https://github.com/tevino/cloudi',
     license='MIT',
-    description='Convenient online En<->Zh dictionary for command line users.',
-    long_description=open('README.rst').read(),
+    description='Convenient online En<->Zh dictionary for command line users.'
 )
